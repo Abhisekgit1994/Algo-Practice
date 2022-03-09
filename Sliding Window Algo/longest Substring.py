1b88890bf6c59d38bacf225c7e03eb4ff75d3057
Abhisekgit1994 <a.panigrahi.1994@gmail.com>
2022-03-08 23:49:29 -0500

    update

--- Sliding Window Algo/longest Substring.py
+++ Sliding Window Algo/longest Substring.py
@@ -23,16 +23,7 @@ class Substring:
         return maxString
 
     def slidingWindow(self, s, k):  # Time complexity O(n), Space Complexity O(1)
-        n = len(s)
-        maxString = ''
-        for i in range(1, n):
-            curr_str = s[:i + 1]  # so that it takes the last character too
-            if len(set(curr_str)) <= k and len(str(curr_str)) > len(
-                    str(maxString)):  # replace with max string as long as the number of unique characters in our current string is less than equal k
-                maxString = max(curr_str, maxString)
-            else:
-                s = s[1:]  # shrink from left if the curr_str out of constraint
-        return maxString
+        pass
 
 
 if __name__ == '__main__':
@@ -43,5 +34,5 @@ if __name__ == '__main__':
         print(f"******************TEST CASE {t}*********************")
         print(f"Output of running all the algos on the string {each} are:")
         print("Simple Approach:", s.simpleApproach(each, 2))
-        print("Sliding window Solution", s.slidingWindow(each, 2))
+        # print("Sliding window Solution", s.slidingWindow(each, 2))
         t += 1
